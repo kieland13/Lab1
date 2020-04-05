699ef48f52b748dad06bf237625f14144592b53e
kieland13 <nkieland@icloud.com>
2020-04-05 18:38:29 -0500

    midterm update

--- MidtermProject/nbaPlayers.js
+++ MidtermProject/nbaPlayers.js
@@ -4,7 +4,7 @@ let resultsContainer = document.querySelector('#results')
 let positionresults = document.querySelector('#positionResults')
 let fheight = document.querySelector('#feetHeight')
 let iheight = document.querySelector('#inchesHeight')
-let teamresults = document.querySelector('#teamResults')
+let team = document.querySelector('#teamResults')
 
 let url = 'https://www.balldontlie.io/api/v1/players?search='
 
@@ -12,11 +12,9 @@ searchButton.addEventListener('click', function() {
     let name = nameInput.value
     let searchUrl = url + name
     resultsContainer.innerHTML = ''
-    positionresults.innerHTML = ''
 
-    fetch(searchUrl)
-        .then( data => data.json() )
-        .then( json => {
+    fetch(searchUrl).then( data => data.json()).then( json =>
+        {
             let allPlayers = json.data
             if (allPlayers.length == 0) {
                 resultsContainer.innerHTML = 'Sorry no results.'
@@ -25,10 +23,6 @@ searchButton.addEventListener('click', function() {
                 allPlayers.forEach( player => {
                     let playerContainer = document.createElement('p')
                     playerContainer.innerHTML = `${player.first_name} ${player.last_name}`
-
-                    let positionContainer = document.createElement('p')
-                    positionContainer.innerHTML = `${player.position}`
-                    positionresults.appendChild(positionContainer)
                     
                     resultsContainer.appendChild(playerContainer)
 
