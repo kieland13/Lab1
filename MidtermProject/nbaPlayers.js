110fecddaeabd8512520ce0f47161b72dabbd1fc
kieland13 <nkieland@icloud.com>
2020-04-05 20:27:40 -0500

    midterm update

--- MidtermProject/nbaPlayers.js
+++ MidtermProject/nbaPlayers.js
@@ -4,6 +4,7 @@ let resultsContainer = document.querySelector('#results')
 let positionresults = document.querySelector('#positionResults')
 let fheight = document.querySelector('#feetHeight')
 let iheight = document.querySelector('#inchesHeight')
+let weight = document.querySelector('#weight')
 let team = document.querySelector('#teamResults')
 
 let url = 'https://www.balldontlie.io/api/v1/players?search='
@@ -12,6 +13,11 @@ searchButton.addEventListener('click', function() {
     let name = nameInput.value
     let searchUrl = url + name
     resultsContainer.innerHTML = ''
+    positionresults.innerHTML = ''
+    fheight.innerHTML = ''
+    iheight.innerHTML = ''
+    weight.innerHTML = ''
+    team.innerHTML = ''
 
     fetch(searchUrl).then( data => data.json()).then( json =>
         {
@@ -22,9 +28,55 @@ searchButton.addEventListener('click', function() {
             else {
                 allPlayers.forEach( player => {
                     let playerContainer = document.createElement('p')
+                    let positionContainer = document.createElement('p')
+                    let feetContainer = document.createElement('p')
+                    let inchContainer = document.createElement('p')
+                    let weightContainer = document.createElement('p')
+                    let teamContainer = document.createElement('p')
+
                     playerContainer.innerHTML = `${player.first_name} ${player.last_name}`
-                    
                     resultsContainer.appendChild(playerContainer)
+
+                    positionContainer.innerHTML = `${player.position}`
+
+                    if (positionContainer.innerHTML.length == 0)
+                    {
+                        positionContainer.innerHTML = "-"
+                    }
+
+                    positionresults.appendChild(positionContainer)
+
+
+
+                    feetContainer.innerHTML = `${player.height_feet}`
+
+                    if (feetContainer.innerHTML == 'null')
+                    {
+                        feetContainer.innerHTML = "-"
+                    }
+
+                    fheight.appendChild(feetContainer)
+
+                    inchContainer.innerHTML = `${player.height_inches}`
+
+                    if (inchContainer.innerHTML == 'null')
+                    {
+                        inchContainer.innerHTML = "-"
+                    }
+
+                    iheight.appendChild(inchContainer)
+
+                    weightContainer.innerHTML = `${player.weight_pounds}`
+
+                    if (weightContainer.innerHTML == 'null')
+                    {
+                        weightContainer.innerHTML = "-"
+                    }
+
+                    weight.appendChild(weightContainer)
+
+                    teamContainer.innerHTML = `${player.team.full_name}`
+                    team.appendChild(teamContainer)
 
                 })
             }
